d0ae43a6dfd4e3d6e9d43376388f96c3b8b2c57e
Rhys Postans <rmp71171@icloud.com>
2024-05-25 17:04:46 +0100

    deploy check

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
       <img
         src="/Phone.svg"
         alt="Header Image"
-        className="w-1/1 sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 z-1"
+        className="w-1/1 sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3"
       />
     </header>
   );
