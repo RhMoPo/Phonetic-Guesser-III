68294da00fc88d0672b11bb45216863c1e8a3fed
Rhys Postans <rmp71171@icloud.com>
2024-05-26 14:25:36 +0100

    styling header -responsivness

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,9 @@ const Header: React.FC = () => {
   return (
     <header className="w-full fixed top-2 z-10 flex justify-center">
       <img
-        src="/Phone.svg"
+        src="/Header.svg"
         alt="Header Image"
-        className="w-1/1 sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3"
+        className="w-1/2 sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 z-1"
       />
     </header>
   );
